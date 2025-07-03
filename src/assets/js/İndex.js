85e29486c50c2e830709992837a9c7793a88c91b
mustafa.oguz <m.sabri.oguz@gmail.com>
2025-07-03 09:59:38 +0300

    505 Hata Sayfası

---
+++ src/assets/js/İndex.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Dark/Light mode toggle
+  const themeToggle = document.getElementById("theme-toggle");
+  const html = document.documentElement;
+
+  // Check for saved user preference or use system preference
+  const savedTheme =
+    localStorage.getItem("theme") ||
+    (window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light");
+  html.classList.toggle("dark", savedTheme === "dark");
+
+  themeToggle.addEventListener("click", () => {
+    html.classList.toggle("dark");
+    const theme = html.classList.contains("dark") ? "dark" : "light";
+    localStorage.setItem("theme", theme);
+  });
+});
